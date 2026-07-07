c9bd6a8e19222eef00923667e7d5c3eb33222a22
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-07-07 13:58:16 +0530

    Codeship: Added solution for Concatenate Non Zero Digits And Multiply By Sum I

---
+++ problems/concatenate-non-zero-digits-and-multiply-by-sum-i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long concatenateAndMultiply(int n) {
+        string digits = to_string(n);
+        string nonZero = "";
+        long long sum = 0;
+        
+        for (char c : digits) {
+            int d = c - '0';
+            sum += d;
+            if (d != 0) {
+                nonZero += c;
+            }
+        }
+        
+        long long x = nonZero.empty() ? 0 : stoll(nonZero);
+        return x * sum;
+    }
+};
