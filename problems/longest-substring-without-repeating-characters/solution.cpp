8fbc62bdd28e6241346f4c15fa76c41bd3754f40
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-07-10 12:06:00 +0530

    Codeship: Added solution for Longest Substring Without Repeating Characters

--- problems/longest-substring-without-repeating-characters/solution.cpp
+++ problems/longest-substring-without-repeating-characters/solution.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> seen;
-        
+        int maxL  =0, l =0;
+
+        for( int r =0; r <s.length(); r++){
+            while(seen.count(s[r])){
+                seen.erase(s[l]);
+                l++;
+            }
+            seen.insert(s[r]);
+            maxL = max(maxL,r-l+1);
+        }
+        return maxL;
     }
 };
