3a30fee09968838285bebfbf001183e174bfd631
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-07-20 23:53:43 +0530

    Codeship: Added solution for Shift 2d Grid

---
+++ problems/shift-2d-grid/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = m * n;
+        k = k % total;  // avoid unnecessary large shifts
+        
+        vector<int> flat(total);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                int newIdx = (idx + k) % total;
+                flat[newIdx] = grid[i][j];
+            }
+        }
+        
+        vector<vector<int>> result(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = flat[i * n + j];
+            }
+        }
+        
+        return result;
+    }
+};
