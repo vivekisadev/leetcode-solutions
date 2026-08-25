cbbc58587dfb83fca069a2807d23881a6eef733e
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-08-26 00:10:19 +0530

    Codeship: Added solution for Find The Duplicate Number

--- problems/find-the-duplicate-number/solution.cpp
+++ problems/find-the-duplicate-number/solution.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> count;
-        for (int num : nums) {
-            count[num]++;
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        for (const auto& pair : count) {
-            if (pair.second >= 2) {
-                return pair.first;
-            }
-        }
-
-        return 0;
+        return slow;
     }
 };
