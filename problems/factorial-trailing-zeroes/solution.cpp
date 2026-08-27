b173a3a45c655ccd138c687a7dc3dae5de0ed06b
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-08-27 23:05:43 +0530

    Codeship: Added solution for Factorial Trailing Zeroes

--- problems/factorial-trailing-zeroes/solution.cpp
+++ problems/factorial-trailing-zeroes/solution.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        long long fac  = 1;
-        for(int i = n; i > 0; i--){
-            fac *= i;
-        }
-        int trailZeros = 0;
-        while(fac > 0) {
-            if(fac % 10 == 0) {
-                trailZeros++;
-                fac /= 10;
-            }
-            else break;
-            
-        }
-
-        return trailZeros;
+        return n/3125+n/625+n/125+n/25+n/5;
     }
 };
