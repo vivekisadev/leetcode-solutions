cfb8af3bc00af318526b9de03da68277a5d50ae5
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-09-04 11:26:51 +0530

    Codeship: Added solution for Smallest Stable Index I

---
+++ problems/smallest-stable-index-i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suffixMin(n);
+        suffixMin[n-1] = nums[n-1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+        }
+
+        int prefixMax = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            prefixMax = max(prefixMax, nums[i]);
+            if( prefixMax - suffixMin[i] <= k) return i;
+        }
+        return -1;
+    }
+};
