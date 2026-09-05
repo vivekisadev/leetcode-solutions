a044a9baeaec83e9e4f745264bd9b3fe17e27985
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-09-05 13:05:27 +0530

    Codeship: Added solution for Smallest Stable Index Ii

--- problems/smallest-stable-index-ii/solution.cpp
+++ problems/smallest-stable-index-ii/solution.cpp
@@ -9,7 +9,7 @@ public:
             suffixMin[i] = min(suffixMin[i + 1], nums[i]);
         }
 
-        int prefixMax = INT_MIN;
+        int prefixMax = -1;
         for(int i = 0; i < n; i++) {
             prefixMax = max(prefixMax, nums[i]);
             if( prefixMax - suffixMin[i] <= k) return i;
