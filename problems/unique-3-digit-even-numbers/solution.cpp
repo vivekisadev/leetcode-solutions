7d8f736d46e4221824659181fb9d962c2cbfadcd
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-09-12 02:27:48 +0530

    Codeship: Added solution for Unique 3 Digit Even Numbers

--- problems/unique-3-digit-even-numbers/solution.cpp
+++ problems/unique-3-digit-even-numbers/solution.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> st;
-        int n = digits.size();
+        vector<int> result;
+        vector<int> mp(10, 0);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                for(int k = 0; k < n; k++) {
-                    if(i == j || i == k || j == k) {
-                        continue;
-                    }
-
-                    int num = digits[i]*100 + digits[j]*10 + digits[k];
-
-                    if(num >= 100 && num%2 == 0) {
-                        st.insert(num);
-                    }
-                }
-            }
+        for(int &digit : digits) {
+            mp[digit]++;
         }
 
-        return st.size();
-    }
+        for(int i = 1; i <= 9; i++) {
+            if(mp[i] == 0) continue;
+            mp[i]--;
 
-    
+            for(int j = 0; j <= 9; j++) {
+                if(mp[j] == 0) continue;
+                mp[j]--;
+
+                for(int k = 0; k <= 8; k += 2) {
+                    if(mp[k] == 0) continue;
+                    mp[k]--;
+
+                    int num = i*100 + j*10 + k;
+                    result.push_back(num);
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        return result.size();
+    }
 };
